10d67a716f0fa1b7674a8625d361ae519e3f789a
Kruti Baraiya <baraiyakruti@gmail.com>
2021-03-08 22:57:16 +0530

    minor changes

--- Vector Line/main.cpp
+++ Vector Line/main.cpp
@@ -6,14 +6,13 @@
 using namespace std;
 
 void myInit() {
-    glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D(-250, 250, -250, 250);
 }
 
 void myDisplay() {
-
+    glClear(GL_COLOR_BUFFER_BIT);
     for(int i = -1000; i < 1000; i+=20) {
         for(int j = -1000; j < 1000; j+=20) {
             BresenhamLine bresenhamLine = BresenhamLine(Point(i,j),Point(i - j,j + i));
@@ -31,8 +30,8 @@ int main(int argc, char **argv) {
     glutInitWindowSize(1000, 1000);
     glutInitWindowPosition(-500, -500);
     glutCreateWindow("Vector Field visualization");
-    myInit();
     glutDisplayFunc(myDisplay);
+    myInit();
 
 
     glutMainLoop();
