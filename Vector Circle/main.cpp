29d5abd1df61104bf5f94ea18bfc95db3c42654f
Kruti Baraiya <baraiyakruti@gmail.com>
2021-03-09 01:01:45 +0530

    minor changes

--- Vector Circle/main.cpp
+++ Vector Circle/main.cpp
@@ -7,12 +7,13 @@ using namespace std;
 
 void myInit() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClearColor(1.0, 1.0, 0.0, 1.0);
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D(-500, 500, -500, 500);
 }
 
 void myDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
     for(int i = -1000; i < 1000; i+=50) {
         for(int j = -1000; j < 1000; j+=50) {
             BresenhamCircle midPointCircle = BresenhamCircle(Point((i+j)/2,(j-i)/2),sqrt(2*(i*i + j*j)));
@@ -29,8 +30,8 @@ int main(int argc, char **argv) {
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
     glutInitWindowSize(1000, 1000);
     glutInitWindowPosition(-500, 500);
-    glutCreateWindow("Bresenham's Circle Drawing");
-    myInit();
+    glutCreateWindow("Vector Circle Visualization");
     glutDisplayFunc(myDisplay);
+    myInit();
     glutMainLoop();
 }
