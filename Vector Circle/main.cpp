318d0235941a86809fc122710f9cfb1781c247cb
Jay Patel <pjay20301@gmail.com>
2021-03-08 18:29:14 +0530

    Header file added

--- Vector Circle/main.cpp
+++ Vector Circle/main.cpp
@@ -1,73 +1,35 @@
 #include <GL/glut.h>
 #include <bits/stdc++.h>
-using namespace std;
+#include "../Point.h"
+#include "BresenhamCircle.h"
 
+using namespace std;
 
 void myInit() {
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(-250, 250,-250 , 250);
-}
-
-void draw_pixel(int x, int y) {
-    glColor3f(0,0,1);
-    glPointSize(2);
-    glBegin(GL_POINTS);
-    glVertex2i(x, y);
-    glEnd();
-}
-
-void draw_circle(int x, int y, int radius) {
-    x = 0;
-    y = radius;
-    double d = 1 - radius;
-    int deltaE = 3;
-    int deltaSE = -2*radius + 5;
-
-    draw_pixel(x,y);
-    while(y > x) {
-        if(d < 0) {
-            d += deltaE;
-            deltaE += 2;
-            deltaSE += 2;
-        } else {
-            d += deltaSE;
-            deltaE += 2;
-            deltaSE += 4;
-            y--;
-        } x++;
-        draw_pixel(x,y);
-        draw_pixel(x,-y);
-        draw_pixel(-x,-y);
-        draw_pixel(-x,y);
-        draw_pixel(y,x);
-        draw_pixel(-y,x);
-        draw_pixel(-y,-x);
-        draw_pixel(y,-x);
-    }
-
-
-
+    gluOrtho2D(-500, 500, -500, 500);
 }
 
 void myDisplay() {
-    for(int i = -100; i < 100; i+=10) {
-        for(int j = -100; j < 100; j+=10) {
-            draw_circle((i+j)/2,(j-i)/2,sqrt(2*(i*i + j*j)));
+    for(int i = -1000; i < 1000; i+=50) {
+        for(int j = -1000; j < 1000; j+=50) {
+            BresenhamCircle midPointCircle = BresenhamCircle(Point((i+j)/2,(j-i)/2),sqrt(2*(i*i + j*j)));
+            midPointCircle.MidPointCircle();
         }
     }
     glFlush();
 }
 
-int main(int argc, char **argv) {
 
+int main(int argc, char **argv) {
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(-250, -250);
-    glutCreateWindow("Vector Field Circle Visualization");
+    glutInitWindowSize(1000, 1000);
+    glutInitWindowPosition(-500, 500);
+    glutCreateWindow("Bresenham's Circle Drawing");
     myInit();
     glutDisplayFunc(myDisplay);
     glutMainLoop();
